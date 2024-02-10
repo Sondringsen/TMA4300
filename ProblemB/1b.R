2e8b29003ad7d8897119a34aa752a803ba803bfc
Eirik Drage Steen <eirik.steen0910@gmail.com>
2024-02-10 17:09:00 +0100

    Fix 1b.R

--- ProblemB/1b.R
+++ ProblemB/1b.R
@@ -1,9 +1,12 @@
-
 GDensity = function(x, alpha){
-    c = alpha / (1 + exp(1)/alpha)
-    x = ifelse (x <= 0, 0, x)
-    x = ifelse(x > 0 & x < 1, c*x**(alpha-1), x)
-    x = ifelse(x >= 1, c*exp(-x), x)
+    c = 1/((1/alpha) + exp(-1))
+    idx0 = which(x<=0)
+    idx1 = which(x>0&x<1)
+    idx2 = which(x>=1)
+
+    x[idx0] = 0
+    x[idx1] = c*x[idx1]**(alpha-1)
+    x[idx2] = c*exp(-x[idx2])
 
     return(x)
 }
@@ -12,19 +15,19 @@ GSample = function(alpha, n){
   u = runif(n)
   
   # Defines the normalising constant
-#   c = alpha/(1-alpha*exp(-1))
-  c = (alpha*exp(1))/(exp(1)+alpha)
+  # c = alpha/(1-alpha*exp(-1))
+  c = 1/((1/alpha) + exp(-1))
 
   # Uses inverse transform to get the distribution
   x = ifelse (u < c/alpha, 
           (u*alpha/c)**(1/alpha),
-          (-log(-u/c+1/alpha+exp(-1)))
+          -log(-u/c+1/alpha+exp(-1))
   )
   return (x)
 }
 
 FDensity = function(x, alpha){
-    return(1/gamma(alpha))*x**(alpha-1)*exp(-x)
+    return((1/gamma(alpha))*x**(alpha-1)*exp(-x))
 }
 
 RejectionSample = function(n, alpha_g, alpha_f){
@@ -37,7 +40,6 @@ RejectionSample = function(n, alpha_g, alpha_f){
         f = FDensity(x, alpha_f)
         g = GDensity(x, alpha_g)
         c = (exp(1) + alpha_g) / (alpha_g*exp(1)*gamma(alpha_f))
-        # c = (1 - alpha_g*exp(-1))/(alpha_g*gamma(alpha_f))
         alpha = (1/c)*f/g
         u = runif(n)
         idx = which(alpha - u > 0)
@@ -57,7 +59,7 @@ rs = RejectionSample(n, alpha_g, alpha_f)
 true_s = rgamma(n=n, alpha_f, 1.0)
 
 print(mean(rs))
-print(mean(true_s))
-print("@@@")
 print(sd(rs))
+print("@@@")
+print(mean(true_s))
 print(sd(true_s))
