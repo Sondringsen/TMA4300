88bcd9ea8eee787ca15f3c27fd72d004f6b91827
Eirik Drage Steen <eirik.steen0910@gmail.com>
2024-03-15 22:18:05 +0100

    Update to loggamma prior for task2, fix log likelihood for task 3

--- project2/3/3.R
+++ project2/3/3.R
@@ -13,10 +13,15 @@ sigmoid <- function(x) {
 f <- function(p) {
 
   y <- rain$n.rain
-  ncr<- choose(rain$n.years, rain$n.rain)
+  n <- rain$n.years
+  T <- length(y)
 
-  bin_terms <- sum(-log(ncr) - y * log(sigmoid(p$x)) - (rain$n.years - y) * log(1 - sigmoid(p$x)))
-  rw_terms <- -log(1 / p$var**(1 / 2)) + sum(1 / (2 * p$var) * (p$x[-1] - p$x[-length(p$x)])**2)
+  # bin_terms <- - sum(y * log(sigmoid(p$x))) - sum((rain$n.years - y) * log(1 - sigmoid(p$x)))
+
+  # rw_terms <- (length(y) - 1)*log(p$var**(1/2)) + (1/(2*p$var)) * sum((p$x[-1] - p$x[-length(p$x)])**2)
+
+  bin_terms <- -sum(y * log(sigmoid(p$x))) - sum((n - y) * log(1 - sigmoid(p$x)))
+  rw_terms <- (T-1) * log(p$var ** (1/2)) + (1/(2*p$var)) * sum((p$x[-1] - p$x[-length(p$x)])**2)
 
   return(bin_terms + rw_terms)
 }
@@ -25,7 +30,7 @@ f <- function(p) {
 # add 1e-04 to account for the rainless days
 pi <- rain$n.rain / rain$n.years + 1e-04
 # Compute x with the logit function
-x <- log(pi / (1-pi))
+x <- log(pi / (1 - pi))
 # Set the variance
 var <- 0.05
 
