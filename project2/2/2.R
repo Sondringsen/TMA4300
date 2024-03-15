d66fb8a753c959afc6b8909c2da2b3e080808032
Eirik Drage Steen <eirik.steen0910@gmail.com>
2024-03-15 22:18:05 +0100

    Update to loggamma prior for task2, fix log likelihood for task 3

--- project2/2/2.R
+++ project2/2/2.R
@@ -4,16 +4,22 @@
 #     INLA="https://inla.r-inla-download.org/R/stable"),
 #     dep=TRUE)
 
+# Import the INLA library
 library("INLA")
 
+# Load the rain data file
 load(file="/Users/eiriksteen/Personal/school/compstat/projects/TMA4300/project2/data/rain.rda")
 
+# Initialize the timer for timing the computation
 ptm <- proc.time()
 
+# Set the control parameters
 control_inla <- list(strategy = "simplified.laplace", int.strategy = "ccd")
 
-formula1 = n.rain ~ -1 + f(day, model = "rw1", constr = FALSE, hyper = list(prec = list(prior = "gamma", param = c(2, 0.05))))
+# Declare the formula for part a)
+formula1 = n.rain ~ -1 + f(day, model = "rw1", constr = FALSE, hyper = list(prec = list(prior = "loggamma", param = c(2, 0.05))))
 
+# Fit the inla model on formula1
 mod1 <- inla(formula1,
              data = rain,
              Ntrials = n.years,
@@ -21,20 +27,25 @@ mod1 <- inla(formula1,
              family = "binomial",
              control.inla = control_inla)
 
+# Print the elapsed time
+print("ELAPSED TIME=")
 print(proc.time() - ptm)
 
-formula2 = n.rain ~ f(day, model = "rw1", constr = FALSE, hyper = list(prec = list(prior = "gamma", param = c(2, 0.05))))
+# Declare the formula for part c)
+formula2 = n.rain ~ f(day, model = "rw1", constr = FALSE, hyper = list(prec = list(prior = "loggamma", param = c(2, 0.05))))
 
+# Fit the inla model on formula2
 mod2 <- inla(formula2,
              data = rain,
              Ntrials = n.years,
              control.compute = list(config = TRUE),
-             family = "binomial", verbose = TRUE,
+             family = "binomial",
              control.inla = control_inla)
 
-
-mod_nocontrol <- inla(formula1,
-                      data = rain,
-                      Ntrials = n.years,
-                      control.compute = list(config = TRUE),
-                      family = "binomial")
+# # Fit the inla model on formula1 without the control parameters,
+# # to assess the robustness
+# mod_nocontrol <- inla(formula1,
+#                       data = rain,
+#                       Ntrials = n.years,
+#                       control.compute = list(config = TRUE),
+#                       family = "binomial")
