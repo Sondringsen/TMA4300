fa5a739deab884cebafc54aa4b862038c353908a
Eirik Drage Steen <eirik.steen0910@gmail.com>
2024-03-16 17:10:03 +0100

    Add saving of plots

--- project2/2/2.R
+++ project2/2/2.R
@@ -4,9 +4,6 @@ library("INLA")
 # Load the rain data file
 load(file="/Users/eiriksteen/Personal/school/compstat/projects/TMA4300/project2/data/rain.rda")
 
-# Initialize the timer for timing the computation
-ptm <- proc.time()
-
 # Set the control parameters
 control_inla <- list(strategy = "simplified.laplace", int.strategy = "ccd")
 
@@ -17,6 +14,9 @@ formula1 = n.rain ~ -1 + f(
     constr = FALSE, 
     hyper = list(prec = list(prior = "loggamma", param = c(2, 0.05))))
 
+# Initialize the timer for timing the computation
+ptm <- proc.time()
+
 # Fit the inla model on formula1
 mod1 <- inla(formula1,
              data=rain, 
@@ -26,8 +26,11 @@ mod1 <- inla(formula1,
              control.inla=control_inla)
 
 # Print the elapsed time
-print("ELAPSED TIME")
+print("ELAPSED TIME SIMPLIFIED LAPLACE")
 print(proc.time() - ptm)
+
+plot(mod1, single = FALSE)
+dev.print(pdf, file="plots/a.pdf")
 
 # Declare the formula for part c)
 formula2 = n.rain ~ f(
@@ -43,3 +46,24 @@ mod2 <- inla(formula2,
              control.compute=list(config = TRUE),
              family="binomial", 
              control.inla=control_inla)
+
+plot(mod2, single = FALSE)
+dev.print(pdf, file="plots/c.pdf")
+
+control_inla_gaussian <- list(strategy = "gaussian", int.strategy = "ccd")
+
+ptm <- proc.time()
+
+# Model with gaussian strategy
+modg <- inla(formula1,
+             data=rain,
+             Ntrials=n.years,
+             control.compute=list(config = TRUE),
+             family="binomial",
+             control.inla=control_inla_gaussian)
+
+print("ELAPSED TIME GAUSSIAN")
+print(proc.time() - ptm)
+
+plot(mod2, single = FALSE)
+dev.print(pdf, file="plots/gaussian-ccd.pdf")
